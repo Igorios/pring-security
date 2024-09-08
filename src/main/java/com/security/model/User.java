6712c06fe3ceeb172c54ce8cdf671c40fd0c71d0
Igor Borges <borgesigor153@gmail.com>
2024-09-08 13:21:49 -0300

    :sparkles: feat: criando o relacionamento das entidades

---
+++ src/main/java/com/security/model/User.java
@@ -0,0 +1,34 @@
+package com.security.model;
+
+import java.util.List;
+
+import com.security.role.Role;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany
+    private List<Role> roles;
+
+}
