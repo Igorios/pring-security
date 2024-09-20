23de170f7882759bba773d3081082f4f3544d9a9
Igor Borges <borgesigor153@gmail.com>
2024-09-20 20:56:59 -0300

    sparkles: feat: configurando o security:

--- src/main/java/com/security/security/WebSecurityConfig.java
+++ src/main/java/com/security/security/WebSecurityConfig.java
@@ -1,25 +1,36 @@
-package com.security.config;
+package com.security.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
+
+    @Autowired
+    private CustomBasicAuthenticationFilter customBasicAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()) // desabilitando o CSRF
+        return http.csrf(csrf -> csrf.disable()) // desabilitando o CSRF
             .cors(cors -> cors.disable()) // desabilitando o CORS 
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(HttpMethod.POST, "/api/v1/user/**").permitAll()
                 .anyRequest().authenticated() // qualquer outra requisição precisa ser autenticada
-            );
-        return http.build();
+            ).addFilterBefore(customBasicAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
+
+        
     }
 
 }
